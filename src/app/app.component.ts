cbdcc3d3af1e342b9eaf226a7fb9deb49272947a
Ana Vuletić <astrihale@Sigrun.local>
2024-12-03 22:47:40 +0100

    Analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,12 @@ import {FooterComponent} from "./footer/footer.component";
 import {HeaderComponent} from "./header/header.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
+    selector: 'app-root',
+    standalone: true,
     imports: [CommonModule, RouterOutlet, MatToolbar, MatButton, MatCard, MatCardTitle, FooterComponent, HeaderComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'candir-rs';
+    title = 'candir-rs';
 }
